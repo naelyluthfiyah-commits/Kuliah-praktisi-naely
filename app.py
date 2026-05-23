24a82dc7c3f998104ae32d1b96eafc65adad5864
naelyluthfiyah-commits <naelyluthfiyah@gmail.com>
2026-05-23 10:31:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,10 @@ st.write("Hello, *World!* :candy:")
 
 title = st.text_input("Movie title", "Life of Brian")
 st.write("The current movie title is", title)
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
