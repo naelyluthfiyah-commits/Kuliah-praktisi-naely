2985940a086f1823340f06171b821abca8eaa5ae
naelyluthfiyah-commits <naelyluthfiyah@gmail.com>
2026-05-23 10:35:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,10 @@ if st.button("Say hello"):
     st.write("Why hello there")
 else:
     st.write("Goodbye")
+option = st.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone"),
+)
+
+st.write("You selected:", option)
 
