55093e20dc78e71967db9c5622e21bb9922a53ee
naelyluthfiyah-commits <naelyluthfiyah@gmail.com>
2026-05-23 10:12:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
 import streamlit as st
+
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 st.title("This is a title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("Laporan Bulanan")
