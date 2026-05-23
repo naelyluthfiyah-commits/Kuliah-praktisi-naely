985c162503345ead7f53fa9545ce8bba6fa25bd8
naelyluthfiyah-commits <naelyluthfiyah@gmail.com>
2026-05-23 10:07:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 import streamlit as st
 st.title("This is a title")
-st.title("_Streamlit_ is :yellow[cool] :sunglesses:")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("Laporan Bulanan")
 st.subheader("📈 Monthly Expenses")
 st.caption("Made with ❤️ using Streamlit")
